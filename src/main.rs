b57c033d15e51201c030cf46d54280dc82b3adf2
7h3w1zard <93vrs93@gmail.com>
2025-12-03 17:09:54 +0300

    Decode bencoded strings

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use serde_json;
+use std::env;
+
+#[allow(dead_code)]
+fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+    // If encoded_value starts with a digit, it's a number
+    if let Some((len, rest)) = encoded_value.split_once(':') {
+        if let Ok(len) = len.parse::<usize>() {
+            return serde_json::Value::String(rest[..len].to_string());
+        }
+    }
+    panic!("Unhandled encoded value: {}", encoded_value)
+}
+
+// Usage: your_program.sh decode "<encoded_value>"
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let command = &args[1];
+
+    if command == "decode" {
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_value(encoded_value);
+        println!("{}", decoded_value.to_string());
+    } else {
+        println!("unknown command: {}", args[1])
+    }
+}
