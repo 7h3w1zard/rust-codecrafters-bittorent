f777883913152f5f9eadf68500b435182e0fbd37
7h3w1zard <93vrs93@gmail.com>
2025-12-05 13:23:39 +0300

    Parse torrent file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,93 @@
+use anyhow::Context;
+use clap::{Parser, Subcommand};
+use hashes::Hashes;
+use serde::Deserialize;
+use serde_bencode;
 use serde_json;
-use std::env;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    Decode { value: String },
+    Info { torrent: PathBuf },
+}
+
+// Usage: your_program.sh decode "<encoded_value>"
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    match args.command {
+        Command::Decode { value } => {
+            // let v: serde_json::Value = serde_bencode::from_str(&value).unwrap();
+            let v = decode_bencoded_value(&value).0;
+            println!("{v}");
+        }
+        Command::Info { torrent } => {
+            let dot_torrent = std::fs::read(torrent).context("read torrent file")?;
+            let t: Torrent =
+                serde_bencode::from_bytes(&dot_torrent).context("parse torrent file")?;
+            println!("Tracker URL: {}", t.announce);
+            if let Keys::SingleFile { length } = t.info.keys {
+                println!("Length: {length}");
+            } else {
+                todo!()
+            }
+        }
+    }
+
+    Ok(())
+}
+
+/// A torrent file (metainfo file) contains a bencoded dictionary.
+#[derive(Debug, Clone, Deserialize)]
+struct Torrent {
+    /// URL to a "tracker", that keeps track of peers participating in the sharing of a torrent.
+    announce: String,
+
+    /// A dictionary with keys:
+    info: Info,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct Info {
+    /// suggested name to save the file / directory as
+    name: String,
+
+    /// number of bytes in each piece
+    #[serde(rename = "piece length")]
+    piece_length: usize,
+
+    /// concatenated SHA-1 hashes of each piece
+    pieces: Hashes,
+
+    #[serde(flatten)]
+    keys: Keys,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(untagged)]
+enum Keys {
+    SingleFile {
+        /// size of the file in bytes, for single-file torrents
+        length: usize,
+    },
+    MultiFile {
+        files: Vec<File>,
+    },
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct File {
+    length: usize,
+    path: Vec<String>,
+}
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
@@ -58,16 +146,46 @@ fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
     panic!("Unhandled encoded value: {}", encoded_value)
 }
 
-// Usage: your_program.sh decode "<encoded_value>"
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let command = &args[1];
+mod hashes {
+    use serde::{
+        Deserialize, Deserializer,
+        de::{self, Visitor},
+    };
+    use std::fmt;
 
-    if command == "decode" {
-        let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.0.to_string());
-    } else {
-        println!("unknown command: {}", args[1])
+    #[derive(Debug, Clone)]
+    pub(crate) struct Hashes(Vec<[u8; 20]>);
+    struct HashesVisitor;
+
+    impl<'de> Visitor<'de> for HashesVisitor {
+        type Value = Hashes;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("a byte string whose length is a multiple of 20")
+        }
+
+        fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
+        where
+            E: de::Error,
+        {
+            if v.len() % 20 != 0 {
+                return Err(E::custom(format!("length is {}", v.len())));
+            }
+
+            Ok(Hashes(
+                v.chunks_exact(20)
+                    .map(|slice_20| slice_20.try_into().expect("guaranteed to be length 20"))
+                    .collect(),
+            ))
+        }
+    }
+
+    impl<'de> Deserialize<'de> for Hashes {
+        fn deserialize<D>(deserializer: D) -> Result<Hashes, D::Error>
+        where
+            D: Deserializer<'de>,
+        {
+            deserializer.deserialize_bytes(HashesVisitor)
+        }
     }
 }
